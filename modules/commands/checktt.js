9ba5602699fa0548559e3ef465f98b6a92ecbe96
DangCongChua <86234168+DangCongChua@users.noreply.github.com>
2021-08-22 20:08:14 +0700

    Update checktt.js

--- modules/commands/checktt.js
+++ modules/commands/checktt.js
@@ -1,22 +1,22 @@
 module.exports.config = {
-	name: "checktt",
-	version: "1.0.4",
-	hasPermssion: 0,
-	credits: "JRT",
-	description: "Kiểm tra lượt tương tác trong nhóm",
-	commandCategory: "Nhóm",
-	usages: "[all/tag]",
-	cooldowns: 10,
+    name: "checktt",
+    version: "1.0.5",
+    hasPermssion: 0,
+    credits: "Mirai Team",
+    description: "Kiểm tra lượt tương tác trong nhóm",
+    commandCategory: "system",
+    usages: "[all/tag]",
+    cooldowns: 5,
     envConfig: {
-        "maxColumn": 400
+        "maxColumn": 10
     }
 };
 
 module.exports.languages = {
     "vi": {
         "all": "%1/ %2 với %3 tin nhắn\n",
-        "mention": "%1 đứng hạng %2 với %3 tin nhắn",
-        "myself": "Bạn đang đứng hạng %1 với %2 tin nhắn"
+        "mention": "Bé iu %1 đứng hạng %2 với %3 tin nhắn",
+        "myself": "Bé iu của Bot đã hót được %2 tin nhắn và đứng hạng %1 !\nChơi đẹp đi, Spam là chó 😾"
     },
     "en": {
         "all": "%1/ %2 with %3 messages\n",
@@ -27,44 +27,59 @@ module.exports.languages = {
 
 module.exports.run = async function ({ args, api, event, Currencies, getText }) {
     var mention = Object.keys(event.mentions);
-    const data = await api.getThreadInfo(event.threadID);
-    if (args[0] == "all") {
-        var number = 1, msg = "", storage = [], exp = [];
-        for (const value of data.userInfo) storage.push({"id" : value.id, "name": value.name});
-        for (const user of storage) {
-            const countMess = await Currencies.getData(user.id);
-            exp.push({"name" : user.name, "exp": (typeof countMess.exp == "undefined") ? 0 : countMess.exp});
+    try {
+        const data = await api.getThreadInfo(event.threadID);
+        if (args[0] == "all") {
+            var number = 0, storage = [], exp = [];
+            for (const value of data.userInfo) storage.push({"id" : value.id, "name": value.name});
+            for (const user of storage) {
+                const countMess = await Currencies.getData(user.id);
+                exp.push({"name" : user.name, "exp": (typeof countMess.exp == "undefined") ? 0 : countMess.exp});
+            }
+            exp.sort(function (a, b) { return b.exp - a.exp });
+
+            var page = 1;
+            page = parseInt(args[1]) || 1;
+            page < -1 ? page = 1 : "";
+            var limit = 25 ;
+            var msg = "✉ 𝙏𝙊𝙋 𝙏𝙐̛𝙊̛𝙉𝙂 𝙏𝘼́𝘾 𝙏𝙍𝙊𝙉𝙂 𝘽𝙊𝙓:\n";
+            var numPage = Math.ceil(exp.length/limit);
+
+            for(var i = limit*(page - 1); i < limit*(page-1) + limit; i++){
+                if(i >= exp.length) break;
+                let infoUser = exp[i];
+                msg += `${i+1}. ${infoUser.name}: ${infoUser.exp} tin nhắn\n`
+            }
+
+            msg += `               ❧ Trang ${page}/${numPage} ☙\n☞ Dùng "${global.config.PREFIX}checktt all + [số trang]" để xem các trang sau!`
+            return api.sendMessage(msg, event.threadID);
         }
-        exp.sort(function (a, b) { return b.exp - a.exp });
+        else if (mention[0]) {
+            var storage = [], exp = [];
+            for (const value of data.userInfo) storage.push({"id" : value.id, "name": value.name});
 
-        for (const lastData of exp)  msg += getText("all", number++, lastData.name, lastData.exp);
-        return api.sendMessage(msg, event.threadID);
-    }
-    else if (mention[0]) {
-        var storage = [], exp = [];
-        for (const value of data.userInfo) storage.push({"id" : value.id, "name": value.name});
+            for (const user of storage) {
+                const countMess = await Currencies.getData(user.id);
+                exp.push({"name" : user.name, "exp": (typeof countMess.exp == "undefined") ? 0 : countMess.exp, "uid": user.id});
+            }
+            exp.sort(function (a, b) { return b.exp - a.exp });
 
-        for (const user of storage) {
-            const countMess = await Currencies.getData(user.id);
-            exp.push({"name" : user.name, "exp": (typeof countMess.exp == "undefined") ? 0 : countMess.exp, "uid": user.id});
+            const rank = exp.findIndex(info => parseInt(info.uid) == parseInt(mention[0])) + 1;
+            const infoUser = exp[rank - 1];
+            return api.sendMessage(getText("mention", infoUser.name, rank, infoUser.exp), event.threadID);
         }
-        exp.sort(function (a, b) { return b.exp - a.exp });
+        else {
+            var storage = [], exp = [];
+            for (const value of data.userInfo) storage.push({"id" : value.id, "name": value.name});
+            for (const user of storage) {
+                const countMess = await Currencies.getData(user.id);
+                exp.push({"name" : user.name, "exp": (typeof countMess.exp == "undefined") ? 0 : countMess.exp, "uid": user.id});
+            }
+            exp.sort(function (a, b) { return b.exp - a.exp });
 
-        const rank = exp.findIndex(info => parseInt(info.uid) == parseInt(mention[0])) + 1;
-        const infoUser = exp[rank - 1];
-        return api.sendMessage(getText("mention", infoUser.name, rank, infoUser.exp), event.threadID);
-    }
-    else {
-        var storage = [], exp = [];
-        for (const value of data.userInfo) storage.push({"id" : value.id, "name": value.name});
-        for (const user of storage) {
-            const countMess = await Currencies.getData(user.id);
-            exp.push({"name" : user.name, "exp": (typeof countMess.exp == "undefined") ? 0 : countMess.exp, "uid": user.id});
+            const rank = exp.findIndex(info => parseInt(info.uid) == parseInt(event.senderID)) + 1;
+            const infoUser = exp[rank - 1];
+            return api.sendMessage(getText("myself", rank, infoUser.exp), event.threadID);
         }
-        exp.sort(function (a, b) { return b.exp - a.exp });
-
-        const rank = exp.findIndex(info => parseInt(info.uid) == parseInt(event.senderID)) + 1;
-        const infoUser = exp[rank - 1];
-        return api.sendMessage(getText("myself", rank, infoUser.exp), event.threadID);
-    }
+    } catch (e) { return console.log(e) }
 }
